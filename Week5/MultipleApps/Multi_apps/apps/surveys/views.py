7f1c04fc7544963f646c61237a8e09246f3e96d2
nickchic <chicirda2@gmail.com>
2017-10-02 18:41:42 -0400

    Organizing

--- Week5/MultipleApps/Multi_apps/apps/surveys/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-
-def index(request):
-    response = "Placeholder to later display all surveys"
-    return HttpResponse(response)
-
-def new(request):
-    response = "Placeholder for users to add a new survey"
-    return HttpResponse(response)
--- Week6/Survey/apps/survey_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^results', views.results),
-    url(r'^$', views.survey)
-]
